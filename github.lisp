7f4a7189b2fbe0ef225cba0d22e0fda4b27059d3
James S <i@nixeagle.org>
2010-01-28 04:00:34 +0000

    github api location, taken from http://develop.github.com/p/general.html

--- github.lisp
+++ github.lisp
@@ -5,3 +5,10 @@
 
 (in-package :nisp.github)
 
+(defparameter +github-api-url+ "https://github.com/api/v2/"
+  ;; Use only the json interface, we do not want to implement the xml or
+  ;; yaml interfaces.
+  "Github api location.
+This is the same for every call.")
+
+
