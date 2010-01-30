2ec076988307e64376d748b514da3e468253a7a5
James S <i@nixeagle.org>
2010-01-30 08:34:43 +0000

    export SHOW-FOLLOWERS and SHOW-FOLLOWING.

--- package.lisp
+++ package.lisp
@@ -1,6 +1,8 @@
 (in-package :nisp.github-system)
 
 (defpackage #:nisp.github
-  (:use :cl :json :iterate))
+  (:use :cl :json :iterate)
+  (:export :show-followers
+           :show-following))
 
 (in-package :nisp.github)
