e7c18f039b53721f1c740abccc077fb1932db10f
James S <i@nixeagle.org>
2010-02-01 03:52:18 +0000

    Make it very clear that some of the functions here are derived from CL-JSON, but all of these are extensively modified.

--- json.lisp
+++ json.lisp
@@ -1,3 +1,44 @@
+;;; This file modifies the basic behavior of CL-JSON.  All of the
+;;; functions that directly modify and manipulate how cl-json reads
+;;; input are derived from the way CL-JSON does the default handling.
+;;;
+;;; My modifications are pretty extensive, but for completeness:
+;;;
+;;; Specifically
+;;; -  beginning-of-object 
+;;; -  key-add-or-set 
+;;; -  value-add-or-set
+;;; -  accumulator-get-object 
+;;; -  accumulator-add-preserved-key
+;;; Are especially derived from CL-JSON.
+;;;
+;;; CL-JSON's license is included here for completeness.
+;;; 
+;;; (This is the MIT / X Consortium license as taken from 
+;;;  http://www.opensource.org/licenses/mit-license.html)
+;;; 
+;;; Copyright (c) 2006-2008 Henrik Hjelte
+;;; Copyright (c) 2008 Hans Hübner (code from the program YASON)
+;;; 
+;;; Permission is hereby granted, free of charge, to any person obtaining
+;;; a copy of this software and associated documentation files (the
+;;; "Software"), to deal in the Software without restriction, including
+;;; without limitation the rights to use, copy, modify, merge, publish,
+;;; distribute, sublicense, and/or sell copies of the Software, and to
+;;; permit persons to whom the Software is furnished to do so, subject to
+;;; the following conditions:
+;;; 
+;;; The above copyright notice and this permission notice shall be
+;;; included in all copies or substantial portions of the Software.
+;;; 
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+;;; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+;;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 (in-package :nisp.github)
 
 ;;; From Alexandria
